cfe99625a86612ccf03561377c76337de1f435c2
Caroline Tenorio <130055209+Carolthe@users.noreply.github.com>
2024-06-25 13:32:48 +0100

    password-generate

--- password-generate/src/Senhas.jsx
+++ password-generate/src/Senhas.jsx
@@ -4,6 +4,7 @@ export default function Senhas (){
     const [password, setPassword] = useState ("")
     const [copy, setCopy] = useState ("Copiar")
     const [passwordSize, setPasswordSize] = useState ("5")
+    const [showInput, setShowInput] = useState(false)
 
     function copyText (){
         window.navigator.clipboard.writeText(password)
@@ -21,19 +22,28 @@ export default function Senhas (){
     }
     return(
         <div>
-            <h1 className="text-4xl font-bold">Gerador de Senhas</h1>
+            <h1 className="text-4xl font-bold text-white">Gerador de Senhas</h1>
+            <div className="mt-10">
+                <label className="text-white  " htmlFor="showInput">Costumizar tamanho</label>
+                <input className="ml-3" 
+                type="checkbox"
+                id="showInput"
+                value={showInput}
+                onChange={() => setShowInput (currentState => !currentState )}
+                />
+            </div>
+            {showInput ? (
             <div>
-                <label className="p-2" htmlFor="passwordSize">Tamanho:</label>
-                <input className="bg-slate-800 border-2 text-white w-72 h-8 mt-10 p-3" 
+                <label className="p-2 text-white" htmlFor="passwordSize">Tamanho:</label>
+                <input className=" bg-slate-800 border-2 text-white w-72 h-8 mt-10 p-3" 
                 type="number"
                 id="passwordSize" 
                 min={1} 
                 value={passwordSize}
-                onChange={(ev) => setPasswordSize (ev.target.value)}
-                />
+                onChange={(ev) => setPasswordSize (ev.target.value)} />
             </div>
-
-            <button onClick={gerar} className="bg-slate-800 text-white w-56 h-10 mr-20 mt-10" >Gerar senha de {passwordSize} caracteres</button>
+            ) : null}
+            <button onClick={gerar} className="bg-slate-800 text-white w-56 h-10 mr-20 mt-10" >Gerar senha de {showInput ? passwordSize : 8 } caracteres</button>
             <button onClick={copyText} className="bg-slate-800 text-white w-24 h-10">{copy}</button>
             <div className="text-black mt-10 text-2xl font-semibold "> {password} </div>
         </div>
